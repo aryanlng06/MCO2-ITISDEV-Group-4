b364e5a9ed11589eae5230f7f7a37a1df9c195b9
aryanlng06 <137790406+aryanlng06@users.noreply.github.com>
2025-04-05 11:17:40 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname); // creates a new mysqli object and connects it to the database. $conn is now the db
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+
---
+++ user_database.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2025 at 09:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_database`
+--
+CREATE DATABASE user_database;
+USE `user_database`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `inbound_qty` int(11) NOT NULL DEFAULT 0,
+  `outbound_qty` int(11) NOT NULL DEFAULT 0,
+  `current_stock` int(11) NOT NULL,
+  `supplier` varchar(255) NOT NULL,
+  `last_restocked` datetime NOT NULL,
+  `stockpile` int(11) NOT NULL DEFAULT 0 -- Add the stockpile field
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `product_name`, `inbound_qty`, `outbound_qty`, `current_stock`, `supplier`, `last_restocked`, `stockpile`) VALUES
+(1, 'Software Licenses', 50, 0, 50, 'Aryan', '2025-03-24 21:43:23', 50),
+(2, 'raw materials', 0, 0, 0, 'Fran Fran', '2025-03-24 21:41:33', 0);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total_amount` decimal(10,0) NOT NULL,
+  `payment_method` varchar(50) NOT NULL DEFAULT 'Cash',
+  `status` varchar(50) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `username`, `product_id`, `quantity`, `total_amount`, `payment_method`, `status`) VALUES
+(1, 'rish', 2, 1, 80, 'Gcash', 'Pending'),
+(2, 'rish', 3, 2, 40000, 'Gcash', 'Pending'),
+(3, 'rish', 2, 1, 80, 'Gcash', 'Pending'),
+(4, 'Sabrina Carpenter', 3, 1, 20000, 'Gcash', 'Pending'),
+(5, 'Francine Lastimosa', 3, 1, 20000, 'Gcash', 'Pending'),
+(6, 'Sabrina Carpenter', 4, 1, 80, 'Gcash', 'Pending'),
+(7, 'Francine Lastimosa', 5, 1, 40000, 'Cash', 'Pending'),
+(8, 'Fran Fran Lastimosa', 4, 1, 80, 'Cash', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(10) UNSIGNED NOT NULL,
+  `supplier` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `price`, `quantity`, `supplier`) VALUES
+(3, 'Electric Guitar', 500.00, 3, 'Francine'),
+(4, 'Buldak', 80.00, 3, 'Rischa'),
+(5, 'Drums', 40000.00, 2, 'Fritz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `security_question` varchar(255) NOT NULL,
+  `security_answer` varchar(255) NOT NULL,
+  `usertype` enum('owner','manager','staff') NOT NULL DEFAULT 'staff'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `security_question`, `security_answer`, `usertype`) VALUES
+(1, 'Aris ', 'siraris@gmail.com', 'DLSU1234!', 'What is your favorite color?', 'Blue', 'staff'),
+(2, 'Fritz', 'sirfritz@gmail.com', 'GK304!', 'What is your favorite color?', 'Green', 'staff');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
